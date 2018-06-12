6d082da3d026f2e522b26ff3b7c31d07f8de5d39
joonvena <joonvena@gmail.com>
2018-06-12 11:31:39 +0300

    Test commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,60 @@
+import java.util.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        // Tulostetaan listasta viikonpaivat jotka alkaa t kirjaimella.
+
+        String[] vkpaivat = {"maanantai", "tiistai", "keskiviikko", "torstai", "perjantai"};
+
+        for(String s : vkpaivat) {
+            if(s.substring(0, 1).contains("t")) {
+                System.out.println(s);
+            }
+        }
+
+        // "t" kirjaimella alkavien elementtien poisto listasta Iteraattoria käyttäen.
+
+        List<String> viikonpaivat = new LinkedList<>();
+
+        viikonpaivat.add("maantai");
+        viikonpaivat.add("tiistai");
+        viikonpaivat.add("torstai");
+        viikonpaivat.add("perjantai");
+        viikonpaivat.add("ää");
+
+        Iterator<String> iteraattori = viikonpaivat.iterator();
+
+        while(iteraattori.hasNext()) {
+            String vkpaiva = iteraattori.next();
+
+            if(vkpaiva.substring(0, 1).contains("t")) {
+                iteraattori.remove();
+            }
+        }
+
+
+        System.out.println(viikonpaivat);
+
+        // Sorttaa viikonpäivät aakkosjärjestykseen
+
+        Collections.sort(viikonpaivat);
+
+        // Sorttaa viikonpäivät käänteiseen aakkosjärjestykseen
+
+        Collections.sort(viikonpaivat, Collections.reverseOrder());
+
+        System.out.println(viikonpaivat);
+        System.out.println(hexToDec("1a10FF"));
+
+
     }
+
+    public static int hexToDec(final String hexString) {
+        String hex = "a";
+        int decimal = Integer.parseInt(hexString, 16);
+        return decimal;
+    }
+
 }
